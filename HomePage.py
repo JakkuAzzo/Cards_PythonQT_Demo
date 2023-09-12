254be7009f1949ca867c66fa53112525288c9334
Nathan <46282855+NathanBrownBennett@users.noreply.github.com>
2023-09-12 11:34:45 +0100

    v 0.0.2

--- HomePage.py
+++ HomePage.py
@@ -1,13 +1,26 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QSpacerItem, QSizePolicy
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSpacerItem
 from NavBar import NavBar
 
 class HomePage(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
+
+        # Title Label
+        title_label = QLabel("Home Page")
+        title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(title_label)
+
+        # Add spacer at the top to push the title and navigation bar down
         spacer_top = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
         layout.addItem(spacer_top)
+
+        # Content for Home Page
+        content_label = QLabel("Welcome to the Home Page!")
+        content_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(content_label)
+
         self.nav_bar = NavBar()
         layout.addWidget(self.nav_bar)
-        
+
         self.setLayout(layout)
--- LibraryPage.py
+++ LibraryPage.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton, QMessageBox
+from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton, QMessageBox, QLabel, QSpacerItem, QSizePolicy
+from NavBar import NavBar
 
 class LibraryPage(QWidget):
     def __init__(self):
@@ -6,7 +7,7 @@ class LibraryPage(QWidget):
         layout = QGridLayout()
         self.setLayout(layout)
         self.populate_library()
-
+        
     def populate_library(self):
         # Simulate library content
         for i in range(3):
@@ -20,3 +21,19 @@ class LibraryPage(QWidget):
         message_box.setWindowTitle("Library Item Clicked")
         message_box.setText("Details about the selected library item.")
         message_box.exec()
+        
+    title_label = QLabel("Library")
+    title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    layout.addWidget(title_label, 0, 0, 1, 3)
+    # Add spacer at the top to push the title and navigation bar down
+    spacer_top = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+    layout.addItem(spacer_top, 1, 0, 1, 3)
+    # Content for Library Page
+    for i in range(3):
+        for j in range(3):
+            library_item = QLabel(f'Library Item {i*3 + j + 1}')
+            library_item.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            layout.addWidget(library_item, i+2, j)
+    self.nav_bar = NavBar()
+    layout.addLayout(self.nav_bar, 5, 0, 1, 3)
+    self.setLayout(layout)
